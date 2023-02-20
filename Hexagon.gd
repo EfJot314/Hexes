711704f1cf32bfed5f09e8ca9ab6e60225c06245
EfJot321 <115103209+EfJot321@users.noreply.github.com>
2023-02-20 15:16:41 +0100

    nie uzywam draw tylko tworze polygon2d

--- Hexagon.gd
+++ Hexagon.gd
@@ -5,33 +5,30 @@ export var X = 0
 export var Y = 0
 export var r = 50
 
-var dr = 2
+var dr = 3
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-func _draw():
 	createHex()
 
+func _draw():
+	#createHex()
+	pass
+
 func createHex():
-	var R = r
-	#obramowanie
+	var R = r-dr
 	var vertices = PoolVector2Array()
 	for i in range(6):
 		var xi = X + R*cos(i*PI/3)
 		var yi = Y + R*sin(i*PI/3)
 		vertices.append(Vector2(xi, yi))
-	draw_polygon(vertices, [Color.black])
-	#wypelnienie
-	vertices = PoolVector2Array()
-	R -= dr
-	for i in range(6):
-		var xi = X + R*cos(i*PI/3)
-		var yi = Y + R*sin(i*PI/3)
-		vertices.append(Vector2(xi, yi))
-	draw_polygon(vertices, [Color.white])
+	var polyg = Polygon2D.new()
+	polyg.polygon += vertices
+	polyg.color = Color.gray
+	self.add_child(polyg)
+	
+	
 		
 	
 
